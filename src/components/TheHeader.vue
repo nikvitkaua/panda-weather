b296429e437a935be95593ef9eb06fb704274522
Mykyta Paziuka <nikvitka.it@gmail.com>
2024-11-27 10:50:46 +0200

    add base structure to proj

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+</script>
+
+<template>
+  <header class="header">
+    <div class="container">
+      <h1 class="logo">Panda Weather</h1>
+      <nav>
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/favorites">Favorites</a></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+
+</style>
