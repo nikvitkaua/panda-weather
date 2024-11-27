e444d1e20f1f72a6e14581de4666cf80b71b0fef
Mykyta Paziuka <nikvitka.it@gmail.com>
2024-11-27 11:24:12 +0200

    finish header and search input

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <script setup>
 import TheHeader from '@/components/TheHeader.vue'
+import AppSearch from '@/components/AppSearch.vue'
 </script>
 
 <template>
-  <TheHeader />
+  <div class="container">
+    <TheHeader />
+    <main>
+      <AppSearch />
+    </main>
+  </div>
 </template>
 
 <style scoped>
